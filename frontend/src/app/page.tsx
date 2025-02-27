69a4e36aeb25d25324432bbc58ccd34f26a0af2e
Gyeonghun Min <riverpower6@naver.com>
2025-02-27 16:45:57 +0900

    [FEAT] [FE] 테일윈드 특수 기능 on, group-on, perr-on

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,20 +1,5 @@
-import { Button } from "@/components/ui/button";
-import { MessageCircle } from "lucide-react";
+import ClientPage from "./ClientPage";
 
 export default function Page() {
-  const socialLoginForKakaoUrl = `http://localhost:8080/oauth2/authorization/kakao`;
-  const redirectUrlAfterSocialLogin = "http://localhost:3000";
-
-  return (
-    <div className="felx-1 flex justify-center items-center">
-      <Button variant="outline" asChild>
-        <a
-          href={`${socialLoginForKakaoUrl}?redirectUrl=${redirectUrlAfterSocialLogin}`}
-        >
-          <MessageCircle />
-          <span className="font-bold">카카오 로그인</span>
-        </a>
-      </Button>
-    </div>
-  );
+  return <ClientPage />;
 }
