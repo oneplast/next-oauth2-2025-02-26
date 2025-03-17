8f4e40d3329828b2621f2add62da5dc7ea660b06
Gyeonghun Min <riverpower6@naver.com>
2025-03-17 11:24:44 +0900

    [REFACTOR] [FE] nextConfig : domains -> remotePatterns

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     dangerouslyAllowSVG: true,
-    domains: ["img1.kakaocdn.net", "placehold.co"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "k.kakaocdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "k.kakaocdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
     contentSecurityPolicy: "default-src 'self'; img-src 'self' data: https:;",
   },
   /* config options here */
